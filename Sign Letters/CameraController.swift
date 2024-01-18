aa4d197c51e3055867cd8bd66add133d9c909243
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-01-18 22:17:52 +0100

    add an ablity to create a MLMultiArray out of a buffored pixels

--- Sign Letters/CameraController.swift
+++ Sign Letters/CameraController.swift
@@ -1,5 +1,6 @@
 import AVFoundation
 import CoreImage
+import CoreML
 class CameraController: NSObject, ObservableObject{
     @Published var frame:CGImage?
     private var permisionGranted = false
@@ -46,10 +47,13 @@ class CameraController: NSObject, ObservableObject{
         videoOutput.connection(with: .video)?.videoRotationAngle = 90
     }
 }
-
+//MARK: Getting camera frames and sending them to a CameraView
 extension CameraController: AVCaptureVideoDataOutputSampleBufferDelegate{
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let cgImage = imageFromSampleBuffer(sampleBuffer: sampleBuffer) else {return}
+        let mlMultiArray = processImage(cgImage)
+        
+        
         
         DispatchQueue.main.async{[unowned self] in
             self.frame = cgImage
@@ -64,4 +68,42 @@ extension CameraController: AVCaptureVideoDataOutputSampleBufferDelegate{
         return cgImage
     }
     
+}
+
+extension CameraController {
+    func processImage(_ image: CGImage) -> MLMultiArray? {
+        let ciImage = CIImage(cgImage: image)
+        
+        // Convert image to grayscale
+        let grayScaleImage = ciImage.applyingFilter("CIPhotoEffectMono")
+        
+        // Resize image to 28x28
+        let scale = CGAffineTransform(scaleX: 28 / CGFloat(image.width), y: 28 / CGFloat(image.height))
+        let scaledImage = grayScaleImage.transformed(by: scale)
+        
+        // Normalize pixel values to be between 0 and 1
+        let normalizedImage = scaledImage.applyingFilter("CIColorControls", parameters: ["inputBrightness": 0, "inputContrast": 1, "inputSaturation": 0])
+        
+        // Create MLMultiArray
+        guard let mlMultiArray = try? MLMultiArray(shape: [1, 28, 28, 1], dataType: .float32) else {
+            return nil
+        }
+        
+        // Assign pixel values to MLMultiArray
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(normalizedImage, from: normalizedImage.extent) else {
+            return nil
+        }
+        let pixelData = cgImage.dataProvider!.data!
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        for y in 0..<cgImage.height {
+            for x in 0..<cgImage.width {
+                let pixelIndex = y * cgImage.width + x
+                let pixelValue = Float(data[pixelIndex]) / 255.0
+                mlMultiArray[[0, y, x, 0] as [NSNumber]] = NSNumber(value: pixelValue)
+            }
+        }
+        
+        return mlMultiArray
+    }
 }
